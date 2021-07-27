5ad0c333351502f5cf93e91489ac8c7c287e83f4
kogisin <jaybkim2@gmail.com>
2021-07-27 17:56:32 +0900

    remove unused rest code

--- x/farming/client/proposal_handler.go
+++ x/farming/client/proposal_handler.go
@@ -4,10 +4,9 @@ import (
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 
 	"github.com/tendermint/farming/x/farming/client/cli"
-	"github.com/tendermint/farming/x/farming/client/rest"
 )
 
 // ProposalHandler is the public plan creation handler.
 var (
-	AddProposalHandler = govclient.NewProposalHandler(cli.GetCmdSubmitPublicPlanProposal, rest.ProposalRESTHandler)
+	AddProposalHandler = govclient.NewProposalHandler(cli.GetCmdSubmitPublicPlanProposal, nil)
 )
