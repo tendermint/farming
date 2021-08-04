8bda489609ef5614a214f9f7d69583f781745e2e
Hanjun Kim <hallazzang@gmail.com>
2021-08-04 14:42:01 +0900

    test: remove some tests that rely on EndBlocker

--- x/farming/keeper/staking_test.go
+++ x/farming/keeper/staking_test.go
@@ -2,8 +2,6 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"github.com/tendermint/farming/x/farming"
 )
 
 func (suite *KeeperTestSuite) TestGetNextStakingID() {
@@ -92,95 +90,6 @@ func (suite *KeeperTestSuite) TestStakingCreationFee() {
 	suite.Require().Error(err)
 }
 
-func (suite *KeeperTestSuite) TestUnstake() {
-	for _, tc := range []struct {
-		name            string
-		addrIdx         int
-		amt             sdk.Coins
-		remainingStaked sdk.Coins
-		remainingQueued sdk.Coins
-		expectErr       bool
-	}{
-		{
-			"from queued coins",
-			0,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 5000)),
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 500_000), sdk.NewInt64Coin(denom2, 1_000_000)),
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 495_000)),
-			false,
-		},
-		{
-			"from staked coins",
-			0,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 700_000), sdk.NewInt64Coin(denom2, 100_000)),
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 300_000), sdk.NewInt64Coin(denom2, 900_000)),
-			sdk.NewCoins(),
-			false,
-		},
-		{
-			"one coin",
-			0,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 1_000_000)),
-			sdk.NewCoins(sdk.NewInt64Coin(denom2, 1_000_000)),
-			sdk.NewCoins(),
-			false,
-		},
-		{
-			"unstake all",
-			0,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 1_000_000), sdk.NewInt64Coin(denom2, 1_000_000)),
-			sdk.NewCoins(),
-			sdk.NewCoins(),
-			false,
-		},
-		{
-			"more than staked",
-			0,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 1_100_000), sdk.NewInt64Coin(denom2, 1_100_000)),
-			// We can use nil since there will be an error and we don't use these fields
-			nil,
-			nil,
-			true,
-		},
-		{
-			"no staking",
-			1,
-			sdk.NewCoins(sdk.NewInt64Coin(denom1, 1000)),
-			nil,
-			nil,
-			true,
-		},
-	} {
-		suite.Run(tc.name, func() {
-			suite.SetupTest()
-			suite.Stake(suite.addrs[0], sdk.NewCoins(
-				sdk.NewInt64Coin(denom1, 500_000),
-				sdk.NewInt64Coin(denom2, 1_000_000)))
-
-			// Make queued coins be staked.
-			suite.ctx = suite.ctx.WithBlockTime(mustParseRFC3339("2021-07-23T05:00:00Z"))
-			farming.EndBlocker(suite.ctx, suite.keeper)
-			suite.ctx = suite.ctx.WithBlockTime(mustParseRFC3339("2021-07-24T00:05:00Z"))
-			farming.EndBlocker(suite.ctx, suite.keeper)
-
-			suite.Stake(suite.addrs[0], sdk.NewCoins(sdk.NewInt64Coin(denom1, 500_000)))
-
-			// At this moment, we have 500,000denom1,1,000,000denom2 staked and
-			// 500000denom1 queued.
-
-			staking, err := suite.keeper.Unstake(suite.ctx, suite.addrs[tc.addrIdx], tc.amt)
-			if tc.expectErr {
-				suite.Error(err)
-			} else {
-				if suite.NoError(err) {
-					suite.True(coinsEq(tc.remainingStaked, staking.StakedCoins))
-					suite.True(coinsEq(tc.remainingQueued, staking.QueuedCoins))
-				}
-			}
-		})
-	}
-}
-
 func (suite *KeeperTestSuite) TestProcessQueuedCoins() {
 	suite.Stake(suite.addrs[0], sdk.NewCoins(sdk.NewInt64Coin(denom1, 1000)))
 
@@ -195,31 +104,4 @@ func (suite *KeeperTestSuite) TestProcessQueuedCoins() {
 
 	suite.Require().True(intEq(sdk.NewInt(1000), staking.StakedCoins.AmountOf(denom1)))
 	suite.Require().True(staking.QueuedCoins.IsZero())
-}
-
-func (suite *KeeperTestSuite) TestEndBlockerProcessQueuedCoins() {
-	suite.Stake(suite.addrs[0], sdk.NewCoins(sdk.NewInt64Coin(denom1, 1000)))
-
-	ctx := suite.ctx.WithBlockTime(mustParseRFC3339("2021-07-23T05:00:00Z"))
-	farming.EndBlocker(ctx, suite.keeper)
-
-	staking, _ := suite.keeper.GetStakingByFarmer(ctx, suite.addrs[0])
-	suite.Require().True(intEq(sdk.NewInt(1000), staking.QueuedCoins.AmountOf(denom1)))
-	suite.Require().True(staking.StakedCoins.IsZero(), "staked coins must be empty")
-
-	suite.Stake(suite.addrs[0], sdk.NewCoins(sdk.NewInt64Coin(denom1, 500)))
-
-	ctx = ctx.WithBlockTime(mustParseRFC3339("2021-07-23T23:59:59Z"))
-	farming.EndBlocker(ctx, suite.keeper)
-
-	staking, _ = suite.keeper.GetStakingByFarmer(ctx, suite.addrs[0])
-	suite.Require().True(intEq(sdk.NewInt(1500), staking.QueuedCoins.AmountOf(denom1)))
-	suite.Require().True(staking.StakedCoins.IsZero(), "staked coins must be empty")
-
-	ctx = ctx.WithBlockTime(mustParseRFC3339("2021-07-24T00:00:01Z"))
-	farming.EndBlocker(ctx, suite.keeper)
-
-	staking, _ = suite.keeper.GetStakingByFarmer(ctx, suite.addrs[0])
-	suite.Require().True(staking.QueuedCoins.IsZero(), "queued coins must be empty")
-	suite.Require().True(intEq(sdk.NewInt(1500), staking.StakedCoins.AmountOf(denom1)))
 }
