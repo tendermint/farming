75b3adddb595c2decdbeedc51ce457258a723417
kogisin <jaybkim2@gmail.com>
2021-08-04 13:22:29 +0900

    feat: add query command-line interface

--- x/farming/client/cli/query.go
+++ x/farming/client/cli/query.go
@@ -3,12 +3,15 @@ package cli
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/version"
 
 	"github.com/tendermint/farming/x/farming/types"
@@ -57,12 +60,12 @@ $ %s query %s params
 
 			queryClient := types.NewQueryClient(clientCtx)
 
-			res, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
+			resp, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
 			if err != nil {
 				return err
 			}
 
-			return clientCtx.PrintProto(&res.Params)
+			return clientCtx.PrintProto(&resp.Params)
 		},
 	}
 
@@ -97,14 +100,14 @@ $ %s query %s plans
 				return err
 			}
 
-			result, err := queryClient.Plans(cmd.Context(), &types.QueryPlansRequest{
+			resp, err := queryClient.Plans(cmd.Context(), &types.QueryPlansRequest{
 				Pagination: pageReq,
 			})
 			if err != nil {
 				return err
 			}
 
-			return clientCtx.PrintProto(result)
+			return clientCtx.PrintProto(resp)
 		},
 	}
 
@@ -116,23 +119,39 @@ $ %s query %s plans
 
 func GetCmdQueryPlan() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "params",
-		Args:  cobra.NoArgs,
-		Short: "Query the values set as liquidity parameters",
+		Use:   "plan [plan-id]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query a plan",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query values set as liquidity parameters.
+			fmt.Sprintf(`Query details about a specific plan.
 
 Example:
-$ %s query %s params
+$ %s query %s plan
 `,
 				version.AppName, types.ModuleName,
 			),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
 
-			// TODO: not implemented yet
+			queryClient := types.NewQueryClient(clientCtx)
 
-			return nil
+			planId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "plan-id %s is not valid", args[0])
+			}
+
+			resp, err := queryClient.Plan(cmd.Context(), &types.QueryPlanRequest{
+				PlanId: planId,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
 		},
 	}
 
@@ -143,52 +162,102 @@ $ %s query %s params
 
 func GetCmdQueryStakings() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "params",
-		Args:  cobra.NoArgs,
-		Short: "Query the values set as liquidity parameters",
+		Use:   "stakings [denom]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query for all stakings",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query values set as liquidity parameters.
+			fmt.Sprintf(`Query details about all farming stakings on a network.
 
 Example:
-$ %s query %s params
+$ %s query %s stakings
 `,
 				version.AppName, types.ModuleName,
 			),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
 
-			// TODO: not implemented yet
-			return nil
+			queryClient := types.NewQueryClient(clientCtx)
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			if err := sdk.ValidateDenom(args[0]); err != nil {
+				return err
+			}
+
+			resp, err := queryClient.Stakings(cmd.Context(), &types.QueryStakingsRequest{
+				StakingCoinDenom: args[0],
+				Pagination:       pageReq,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
 		},
 	}
 
 	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "stakings")
 
 	return cmd
 }
 
 func GetCmdQueryRewards() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "params",
-		Args:  cobra.NoArgs,
-		Short: "Query the values set as liquidity parameters",
+		Use:   "rewards [farmer-addr]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query for all rewards from a farmer",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Query values set as liquidity parameters.
+			fmt.Sprintf(`Query rewards that are accumulated on a network from a farmer.
 
 Example:
-$ %s query %s params
+$ %s query %s rewards %s1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj
 `,
-				version.AppName, types.ModuleName,
+				version.AppName, types.ModuleName, sdk.Bech32MainPrefix,
 			),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
 
-			// TODO: not implemented yet
-			return nil
+			queryClient := types.NewQueryClient(clientCtx)
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			farmerAcc, err := sdk.ValAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			if err := sdk.ValidateDenom(args[0]); err != nil {
+				return err
+			}
+
+			resp, err := queryClient.Rewards(cmd.Context(), &types.QueryRewardsRequest{
+				Farmer:           farmerAcc.String(),
+				StakingCoinDenom: args[0],
+				Pagination:       pageReq,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
 		},
 	}
 
 	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "rewards")
 
 	return cmd
 }
