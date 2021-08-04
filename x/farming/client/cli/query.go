46c6433ce283dbfe9642b49b5647258cd4164595
kogisin <jaybkim2@gmail.com>
2021-08-04 09:30:33 +0900

    feat: adding query command-line interface

--- x/farming/client/cli/query.go
+++ x/farming/client/cli/query.go
@@ -1,8 +1,15 @@
 package cli
 
 import (
-	"github.com/cosmos/cosmos-sdk/client"
+	"context"
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	"github.com/tendermint/farming/x/farming/types"
 )
@@ -18,9 +25,170 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	farmingQueryCmd.AddCommand(
-	// TODO: add query commands
-	// GetCmdQueryPlan(),
+		GetCmdQueryParams(),
+		GetCmdQueryPlans(),
+		GetCmdQueryPlan(),
+		GetCmdQueryStakings(),
+		GetCmdQueryRewards(),
 	)
 
 	return farmingQueryCmd
+}
+
+func GetCmdQueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the current farming parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as farming parameters.
+
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func GetCmdQueryPlans() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "plans",
+		Args:  cobra.NoArgs,
+		Short: "Query for all farming plans",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query details about all farming plans on a network.
+
+Example:
+$ %s query %s plans
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			result, err := queryClient.Plans(cmd.Context(), &types.QueryPlansRequest{
+				Pagination: pageReq,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(result)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "plans")
+
+	return cmd
+}
+
+func GetCmdQueryPlan() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the values set as liquidity parameters",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as liquidity parameters.
+
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// TODO: not implemented yet
+
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func GetCmdQueryStakings() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the values set as liquidity parameters",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as liquidity parameters.
+
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// TODO: not implemented yet
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func GetCmdQueryRewards() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the values set as liquidity parameters",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as liquidity parameters.
+
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// TODO: not implemented yet
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
 }
