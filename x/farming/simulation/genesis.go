d0ae1126536262105ca094196239accc5dabd321
kogisin <jaybkim2@gmail.com>
2021-08-19 15:57:43 +0900

    test: add randomized genesis params

--- x/farming/simulation/genesis.go
+++ x/farming/simulation/genesis.go
@@ -9,22 +9,34 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/types/simulation"
 
 	"github.com/tendermint/farming/x/farming/types"
 )
 
-// Simulation parameter constants
+// Simulation parameter constants.
 const (
 	PrivatePlanCreationFee = "private_plan_creation_fee"
+	StakingCreationFee     = "staking_creation_fee"
+	EpochDays              = "epoch_days"
 )
 
-// GenPrivatePlanCreationFee return default PrivatePlanCreationFee
+// GenPrivatePlanCreationFee return randomized private plan creation fee.
 func GenPrivatePlanCreationFee(r *rand.Rand) sdk.Coins {
-	// TODO: randomize private plan creation fee
-	return types.DefaultPrivatePlanCreationFee
+	return sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(simulation.RandIntBetween(r, 0, 1_000_000_000))))
 }
 
-// RandomizedGenState generates a random GenesisState for farming
+// GenStakingCreationFee return randomized staking creation fee.
+func GenStakingCreationFee(r *rand.Rand) sdk.Coins {
+	return sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(simulation.RandIntBetween(r, 0, 1_000_000_000))))
+}
+
+// GenEpochDays return default EpochDays.
+func GenEpochDays(r *rand.Rand) uint32 {
+	return types.DefaultEpochDays
+}
+
+// RandomizedGenState generates a random GenesisState for farming.
 func RandomizedGenState(simState *module.SimulationState) {
 	var privatePlanCreationFee sdk.Coins
 	simState.AppParams.GetOrGenerate(
@@ -32,9 +44,23 @@ func RandomizedGenState(simState *module.SimulationState) {
 		func(r *rand.Rand) { privatePlanCreationFee = GenPrivatePlanCreationFee(r) },
 	)
 
+	var stakingCreationFee sdk.Coins
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, StakingCreationFee, &stakingCreationFee, simState.Rand,
+		func(r *rand.Rand) { stakingCreationFee = GenStakingCreationFee(r) },
+	)
+
+	var epochDays uint32
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, EpochDays, &epochDays, simState.Rand,
+		func(r *rand.Rand) { epochDays = GenEpochDays(r) },
+	)
+
 	farmingGenesis := types.GenesisState{
 		Params: types.Params{
 			PrivatePlanCreationFee: privatePlanCreationFee,
+			StakingCreationFee:     stakingCreationFee,
+			EpochDays:              epochDays,
 		},
 	}
 
