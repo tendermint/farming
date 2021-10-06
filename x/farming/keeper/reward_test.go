93c00c481640b078759b50dfbc2bcf204b491a9d
kogisin <jaybkim2@gmail.com>
2021-10-06 15:30:03 +0900

    feat: increase code coverage

--- x/farming/keeper/reward_test.go
+++ x/farming/keeper/reward_test.go
@@ -288,12 +288,16 @@ func (suite *KeeperTestSuite) TestHarvest() {
 		suite.keeper.SetPlan(suite.ctx, plan)
 	}
 
+	// should return staking not exists
+	err := suite.keeper.Harvest(suite.ctx, suite.addrs[0], []string{denom1})
+	suite.Require().Error(err)
+
 	suite.Stake(suite.addrs[0], sdk.NewCoins(sdk.NewInt64Coin(denom1, 1_000_000)))
 	suite.keeper.ProcessQueuedCoins(suite.ctx)
 
 	balancesBefore := suite.app.BankKeeper.GetAllBalances(suite.ctx, suite.addrs[0])
 	suite.ctx = suite.ctx.WithBlockTime(types.ParseTime("2021-08-05T00:00:00Z"))
-	err := suite.keeper.AllocateRewards(suite.ctx)
+	err = suite.keeper.AllocateRewards(suite.ctx)
 	suite.Require().NoError(err)
 
 	rewards := suite.keeper.AllRewards(suite.ctx, suite.addrs[0])
