2abdd70c6a9924c1e7c235add01162035af2452c
Hanjun Kim <hallazzang@gmail.com>
2021-10-14 14:23:05 +0900

    fix: remove unused sentinel errors

--- x/farming/types/errors.go
+++ x/farming/types/errors.go
@@ -6,15 +6,12 @@ import (
 
 // farming module sentinel errors
 var (
-	ErrPlanNotExists                  = sdkerrors.Register(ModuleName, 2, "plan does not exist")
-	ErrInvalidPlanType                = sdkerrors.Register(ModuleName, 3, "invalid plan type")
-	ErrInvalidPlanEndTime             = sdkerrors.Register(ModuleName, 4, "invalid plan end time")
-	ErrStakingNotExists               = sdkerrors.Register(ModuleName, 5, "staking not exists")
-	ErrRewardNotExists                = sdkerrors.Register(ModuleName, 6, "reward not exists")
-	ErrFeeCollectionFailure           = sdkerrors.Register(ModuleName, 7, "fee collection failure")
-	ErrInvalidPlanNameLength          = sdkerrors.Register(ModuleName, 8, "invalid plan name length")
-	ErrInvalidPlanName                = sdkerrors.Register(ModuleName, 9, "invalid plan name")
-	ErrConflictPrivatePlanFarmingPool = sdkerrors.Register(ModuleName, 10, "the address is already in use, please use a different plan name")
-	ErrInvalidStakingReservedAmount   = sdkerrors.Register(ModuleName, 11, "staking reserved amount invariant broken")
-	ErrInvalidRemainingRewardsAmount  = sdkerrors.Register(ModuleName, 12, "remaining rewards amount invariant broken")
+	ErrInvalidPlanType                = sdkerrors.Register(ModuleName, 2, "invalid plan type")
+	ErrInvalidPlanEndTime             = sdkerrors.Register(ModuleName, 3, "invalid plan end time")
+	ErrStakingNotExists               = sdkerrors.Register(ModuleName, 4, "staking not exists")
+	ErrInvalidPlanNameLength          = sdkerrors.Register(ModuleName, 5, "invalid plan name length")
+	ErrInvalidPlanName                = sdkerrors.Register(ModuleName, 6, "invalid plan name")
+	ErrConflictPrivatePlanFarmingPool = sdkerrors.Register(ModuleName, 7, "the address is already in use, please use a different plan name")
+	ErrInvalidStakingReservedAmount   = sdkerrors.Register(ModuleName, 8, "staking reserved amount invariant broken")
+	ErrInvalidRemainingRewardsAmount  = sdkerrors.Register(ModuleName, 9, "remaining rewards amount invariant broken")
 )
